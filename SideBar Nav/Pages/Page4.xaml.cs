fcbe3a179f643911e375b151addc98a1271779f8
Mathis1187 <mathisvincent1@gmail.com>
2025-03-12 15:31:41 -0400

    ajout du projet et code side bar

---
+++ SideBar Nav/Pages/Page4.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TheClassMain.Pages
+{
+    /// <summary>
+    /// Interaction logic for Page4.xaml
+    /// </summary>
+    public partial class Page4 : Page
+    {
+        public Page4()
+        {
+            InitializeComponent();
+        }
+    }
+}
