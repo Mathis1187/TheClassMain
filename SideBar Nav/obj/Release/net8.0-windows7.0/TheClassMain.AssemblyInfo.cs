f63f3fb636c441b82cd85944f97188a053dad39b
Mathis1187 <mathisvincent1@gmail.com>
2025-03-17 16:17:58 -0400

    test 3 mars

--- SideBar Nav/obj/Release/net8.0-windows7.0/TheClassMain.AssemblyInfo.cs
+++ SideBar Nav/obj/Release/net8.0-windows7.0/TheClassMain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheClassMain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcbe3a179f643911e375b151addc98a1271779f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06bcdba09c657cd0b787b7e63cccc1e602cd064")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheClassMain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheClassMain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
