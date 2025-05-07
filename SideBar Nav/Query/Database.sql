07800faf5f3bc33f234c1a404a4ccbd971565729
Mathis1187 <mathisvincent1@gmail.com>
2025-05-07 15:57:18 -0400

    modification du nom de la base de donnee

---
+++ SideBar Nav/Query/Database.sql
@@ -0,0 +1,17 @@
+﻿-- 1. Crée la base de données (si elle n'existe pas déjà)
+CREATE DATABASE TheClassMainDB;
+GO
+
+-- 2. Crée un login SQL Server (au niveau du serveur)
+CREATE LOGIN root WITH PASSWORD = '1234';
+GO
+
+-- 3. Associe ce login à un utilisateur dans la base TheClassMainDB
+USE TheClassMainDB;
+GO
+CREATE USER root FOR LOGIN root;
+GO
+
+-- 4. Donne à l'utilisateur les droits de propriétaire de la base
+ALTER ROLE db_owner ADD MEMBER root;
+GO
--- SideBar Nav/Query/TableContext.cs
+++ SideBar Nav/Query/TableContext.cs
@@ -1,22 +1,19 @@
-﻿namespace TheClassMain.Query
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using TheClassMain.Model;
+namespace TheClassMain.Query
 {
-    using Microsoft.EntityFrameworkCore;
-    using TheClassMain.Model;
-
     public class TableContext : DbContext
     {
         public DbSet<Categories> CategoriesT { get; set; }
-
         public DbSet<Factures> FacturesT { get; set; }
-
         public DbSet<Customer> CustomersT { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
-                @"Server=(LocalDB)\MSSQLLocalDB;Database=TheClassMainDB;Trusted_Connection=True",
+                @"Server=(LocalDB)\MSSQLLocalDB;Database=TheClassMainDatabase;Trusted_Connection=True;",
                 options => options.EnableRetryOnFailure());
         }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Factures>()
@@ -24,23 +21,28 @@
                 .WithMany(c => c.Factures)
                 .HasForeignKey(f => f.CustomerId)
                 .OnDelete(DeleteBehavior.Restrict);
-
             modelBuilder.Entity<Categories>()
                 .HasOne(c => c.Customer)
                 .WithMany(cu => cu.Categories)
                 .HasForeignKey(c => c.CustomerId)
                 .OnDelete(DeleteBehavior.Cascade);
-
             modelBuilder.Entity<Factures>()
                 .HasOne(f => f.Categorie)
                 .WithMany(c => c.Factures)
                 .HasForeignKey(f => f.CategorieId)
                 .OnDelete(DeleteBehavior.Restrict);
         }
-
         public void CreateDatabase()
         {
-            this.Database.EnsureCreated();
+            try
+            {
+                this.Database.EnsureCreated();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
