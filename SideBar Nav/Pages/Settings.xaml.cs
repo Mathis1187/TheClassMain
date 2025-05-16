7ed8db25cde5ac9933406fa6c2943c25dc5f1e8e
Elverovski <MarioEnriqueSilvaBenavides2020@gmail.com>
2025-05-15 23:32:37 -0400

    Add fonctionalite qui efface tous les factures - Mario

--- SideBar Nav/Pages/Settings.xaml.cs
+++ SideBar Nav/Pages/Settings.xaml.cs
@@ -1,11 +1,16 @@
 ﻿namespace TheClassMain.Pages
 {
     using System.Windows.Controls;
+    using TheClassMain.ViewModel;
+
     public partial class Settings : Page
     {
+
+        private readonly FactureViewModel viewModel = new();
         public Settings()
         {
             InitializeComponent();
+            DataContext = viewModel;
         }
     }
 }
